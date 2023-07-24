bb56c34a38527d48775afb27c686345016ba79c1
laasyak02 <101655219+laasyak02@users.noreply.github.com>
2023-07-24 21:33:24 +0530

    Create Arduino_Traffic_Signal.py

---
+++ Arduino_Traffic_Signal.py
@@ -0,0 +1,30 @@
+from pyfirmata import Arduino, util
+import time
+board= Arduino('/dev/ttyUSB1')
+led_r = board.digital[10]
+led_y = board.digital[9]
+led_g = board.digital[6]
+iterator = util.Iterator(board)
+iterator.start()
+while True:
+	for i in range(50):
+		led_r.write(1)
+		led_y.write(0)
+		led_g.write(0)
+		time.sleep(0.1)
+	for i in range(20):
+		led_r.write(0)
+		led_y.write(1)
+		led_g.write(0)
+		time.sleep(0.1)
+	for i in range(50):
+		led_r.write(0)
+		led_y.write(0)
+		led_g.write(1)
+		time.sleep(0.1)
+	for i in range(20):
+		led_r.write(0)
+		led_y.write(1)
+		led_g.write(0)
+		time.sleep(0.1)
+board.exit()
